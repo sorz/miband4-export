463d5640c13ef531dad5bf8e196b5358b081203d
sorz <me@sorz.org>
2020-03-31 14:48:03 +0800

    Impl heart rate data export

--- app/src/main/java/cn/edu/sustech/cse/miband/DeviceFragment.kt
+++ app/src/main/java/cn/edu/sustech/cse/miband/DeviceFragment.kt
@@ -14,6 +14,7 @@ import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
 import androidx.work.*
 import cn.edu.sustech.cse.miband.databinding.FragmentDeviceBinding
+import cn.edu.sustech.cse.miband.db.HeartBeatDao
 import cn.edu.sustech.cse.miband.db.RecordDao
 import kotlinx.android.synthetic.main.fragment_device.*
 import kotlinx.coroutines.CoroutineScope
@@ -31,7 +32,8 @@ private const val HEART_BEAT_WORK_NAME = "work-heat-beat"
 class DeviceFragment : Fragment(), AnkoLogger {
     private val args: DeviceFragmentArgs by navArgs()
     private val viewModel: DeviceViewModel by viewModels()
-    private lateinit var recordDao: RecordDao
+    private val recordDao by lazy { requireContext().database.recordDao() }
+    private val heartBeatDao by lazy { requireContext().database.heartBeatDao() }
     private lateinit var miBand: MiBand
     private lateinit var workManager: WorkManager
 
@@ -47,7 +49,6 @@ class DeviceFragment : Fragment(), AnkoLogger {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         miBand = MiBand(requireContext(), args.device, args.key, viewLifecycleOwner)
-        recordDao = requireContext().database.recordDao()
         workManager = WorkManager.getInstance(requireContext())
 
         viewLifecycleOwner.lifecycleScope.launchWhenStarted {
@@ -56,12 +57,14 @@ class DeviceFragment : Fragment(), AnkoLogger {
         }
 
         fetch_button.setOnClickListener { fetchData() }
-        heart_rate_button.setOnClickListener { realtimeHeartRate() }
-        heart_rate_stop_button.setOnClickListener { stopRealtimeHeartRate() }
         monitor_enable_button.setOnClickListener { enableBackgroundHeartRate() }
         monitor_disable_button.setOnClickListener { disableBackgroundHeartRate() }
-        delete_button.setOnClickListener { deleteAllRecords() }
-        export_button.setOnClickListener { exportAsCsv() }
+        delete_activities_button.setOnClickListener { deleteAllRecords() }
+        export_activities_button.setOnClickListener { exportActivitiesAsCsv() }
+        heart_rate_button.setOnClickListener { realtimeHeartRate() }
+        heart_rate_stop_button.setOnClickListener { stopRealtimeHeartRate() }
+        export_heart_button.setOnClickListener { exportHeartAsCsv() }
+        delete_heart_button.setOnClickListener { deleteAllHeartRate() }
 
         workManager.getWorkInfosForUniqueWorkLiveData(HEART_BEAT_WORK_NAME)
             .observe(viewLifecycleOwner, Observer { works ->
@@ -130,17 +133,19 @@ class DeviceFragment : Fragment(), AnkoLogger {
         showSnack("All records removed")
     }
 
-    private fun exportAsCsv() = operateBand {
+    private fun deleteAllHeartRate() = operateBand {
+        heartBeatDao.deleteAll()
+        showSnack("All records removed")
+    }
+
+    private fun exportActivitiesAsCsv() = operateBand {
         val records = recordDao.selectAll()
         if (records.isEmpty()) {
             showSnack("No records found")
             return@operateBand
         }
 
-        val dir = File(requireContext().cacheDir, "exported")
-        dir.mkdirs()
-        val file = File(dir, "activities-${records.last().time}.csv")
-        file.deleteOnExit()
+        val file = createExportCsvFile("activities-${records.last().time}")
         withContext(Dispatchers.IO) {
             val writer = file.outputStream().bufferedWriter()
             writer.write("time,step,heart_beat\r\n")
@@ -150,9 +155,38 @@ class DeviceFragment : Fragment(), AnkoLogger {
                 }
             writer.flush()
             writer.close()
-            file
+        }
+        exportCsvFile(file)
+    }
+
+    private fun exportHeartAsCsv() = operateBand {
+        val records = heartBeatDao.selectAll()
+        if (records.isEmpty()) {
+            showSnack("no records found")
+            return@operateBand
         }
 
+        val file = createExportCsvFile("heartbeat-${records.last().time}")
+        withContext(Dispatchers.IO) {
+            val writer = file.outputStream().bufferedWriter()
+            writer.write("time,bpm\r\n")
+            for (record in records)
+                writer.write("${record.time},${record.bpm}\r\n")
+            writer.flush()
+            writer.close()
+        }
+        exportCsvFile(file)
+    }
+
+    private fun createExportCsvFile(basename: String): File {
+        val dir = File(requireContext().cacheDir, "exported")
+        dir.mkdirs()
+        val file = File(dir, "$basename.csv")
+        file.deleteOnExit()
+        return file
+    }
+
+    private fun exportCsvFile(file: File) {
         val uri = FileProvider.getUriForFile(requireContext(), requireContext().packageName, file)
         val intent = ShareCompat.IntentBuilder.from(requireActivity())
             .setStream(uri)
